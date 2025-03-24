82583dc03f53eaec75f49694672a564538bba032
SamueleTerraroli <terraroli99@gmail.com>
2025-03-24 16:28:42 +0100

    add: somma

---
+++ snack.js
@@ -0,0 +1,18 @@
+/* Sanck 1 Crea una funzione che somma due numeri.
+
+ Crea una funzione dichiarativa chiamata somma che accetta due numeri e restituisce la loro somma.
+function somma(a, b) {
+    return a + b;
+}
+
+Poi, definisci la stessa funzione somma ma come funzione anonima assegnata a una variabile
+const somma = function (a, b) {
+    return a + b;
+}
+
+const somma = (a, b) => a + b;
+
+console.log(somma(2, 3));
+*/
+
+
