2f44fadec56e7a381eaad85f1b42efebe731052a
SamueleTerraroli <terraroli99@gmail.com>
2025-03-24 16:32:07 +0100

    add: quadrato

--- snack.js
+++ snack.js
@@ -15,4 +15,11 @@ const somma = (a, b) => a + b;
 console.log(somma(2, 3));
 */
 
+/* Snack 2 Crea una arrow function che calcola il quadrato di un numero.
 
+Definisci una funzione chiamata quadrato che accetta un numero e restituisce il suo quadrato in una sola riga.
+
+
+*/
+const quadrato = (num) => num * num;
+console.log(quadrato(3));
